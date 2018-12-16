da3c059b3752cad544ed3eaaabc80cfa1b83a146
Sanyog Ghosh <f20160635@goa.bits-pilani.ac.in>
2018-12-16 11:30:54 +0530

    feat : Added Question Track UI

--- practice/urls.py
+++ practice/urls.py
@@ -4,13 +4,19 @@ from . import views
 app_name="practice"
 
 urlpatterns = [
-
+    #below six belong to algorithms
     path('', views.index, name='index'),
-    path('algorithms', views.algorithms, name='algorithms'),
-    path('algorithms1', views.algorithms1, name='algorithms1'),
-    path('data_structures', views.data_structures, name='data_structures'),
-    path('programming_languages', views.programming_languages, name='programming_languages'),
-    path('codemirror', views.codemirror, name='codemirror'),
-    path('solution', views.solution, name='solution'),
+    path('algorithms/algorithms.html', views.algorithms, name='algorithms'),
+    path('algorithms/greedy_algorithms/greedy_algorithms.html', views.greedy_algorithms, name='greedy_algorithms'),
+    path('algorithms/string_algorithms/string_algorithms.html', views.string_algorithms, name='string_algorithms'),
+    path('algorithms/sorting/sorting.html', views.sorting, name='sorting'),
+    path('algorithms/dynamic_programming/dynamic_programming.html', views.dynamic_programming, name='dynamic_programming'),
+    path('algorithms/dynamic_programming/questions/Sherlock and Coprime Subset.html', views.sherlock, name='sherlock'),
+    path('algorithms/graphs/graphs.html', views.graphs, name='graphs'),
+    
+    
+    path('algorithms/dynamic_programming/questions/Simulation.html', views.sim, name='sim'),
+    path('algorithms/dynamic_programming/questions/Explore.html', views.exp, name='exp'),
 
+    path('data_structures', views.data_structures, name='data_structures')
 ]
--- practice/views.py
+++ practice/views.py
@@ -1,28 +1,46 @@
 from django.shortcuts import render
-from .forms import ContactForm
 
 # Create your views here.
 def index(request):
     return render(request, 'practice/index.html', {})
 
 def algorithms(request):
-    return render(request, 'practice/algorithms.html', {})
+    return render(request, 'practice/algorithms/algorithms.html', {})
 
-def algorithms1(request):
-    return render(request, 'practice/algorithms1.html', {})
+#dp problems
+def dynamic_programming(request):
+    return render(request, 'practice/algorithms/dynamic_programming/dynamic_programming.html', {})
+def sherlock(request):
+    return render(request, 'practice/algorithms/dynamic_programming/questions/Sherlock and Coprime Subset.html', {})
+
+def exp(request):
+    return render(request, 'practice/algorithms/dynamic_programming/questions/Explore.html', {})
+
+
+def sim(request):
+    return render(request, 'practice/algorithms/dynamic_programming/questions/Simulation.html', {})
+
+
+
+def graphs(request):
+    return render(request, 'practice/algorithms/graphs/graphs.html', {})
+
+
+
+def greedy_algorithms(request):
+    return render(request, 'practice/algorithms/greedy_algorithms/greedy_algorithms.html', {})
+
+
+
+def string_algorithms(request):
+    return render(request, 'practice/algorithms/string_algorithms/string_algorithms.html', {})
+
+
+
+
+def sorting(request):
+    return render(request, 'practice/algorithms/sorting/sorting.html', {})
+
 
 def data_structures(request):
-    return render(request, 'practice/data_structures.html', {})
-
-def programming_languages(request):
-    return render(request, 'practice/programming_languages.html', {})
-
-def codemirror(request):
-    return render(request, 'practice/codemirror.html', {})
-
-def solution(request):
-    if request.method == 'POST':
-        url = request.POST.get('url')
-    return render(request, 'practice/solution.html', {
-    "url":url
-    })
+    return render(request, 'practice/data_structures/data_structures.html', {})
