9ba414e2278fc435b141edde2126905a97db0e64
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2018-12-15 20:31:08 -0800

    index.py: Fix Dead LInks
    Removes Copyright by mobirize footer and fixes dead links
    in home:index.py and practice:index.py
    Adds Pages c.html, java.html, python.html, data_structures.html
    
    Closes https://github.com/Utkarsh1308/TabOverSpace/issues/3 and https://github.com/Utkarsh1308/TabOverSpace/issues/2

--- practice/urls.py
+++ practice/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('algorithms', views.algorithms, name='algorithms'),
     path('algorithms1', views.algorithms1, name='algorithms1'),
     path('data_structures', views.data_structures, name='data_structures'),
-    path('programming_languages', views.programming_languages, name='programming_languages'),
+    path('python', views.python, name='python'),
+    path('java', views.java, name='java'),
+    path('c++', views.c, name='c'),
     path('codemirror', views.codemirror, name='codemirror'),
     path('solution', views.solution, name='solution'),
 
