0b24709dbbd71a227decd9dcc63b626dc2f97fc5
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2018-12-17 01:09:28 -0800

    Subdomains update CSS and page when clicked
    Closes https://github.com/Utkarsh1308/TabOverSpace/issues/6

--- practice/urls.py
+++ practice/urls.py
@@ -7,12 +7,20 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('algorithms', views.algorithms, name='algorithms'),
-    path('algorithms1', views.algorithms1, name='algorithms1'),
     path('data_structures', views.data_structures, name='data_structures'),
     path('python', views.python, name='python'),
     path('java', views.java, name='java'),
     path('c++', views.c, name='c'),
-    path('codemirror', views.codemirror, name='codemirror'),
-    path('solution', views.solution, name='solution'),
+    path('algorithms/constructive_algorithms', views.constructive_algorithms, name='constructive_algorithms'),
+    path('algorithms/dynamic_programming', views.dynamic_programming, name='dynamic_programming'),
+    path('algorithms/graph_theory', views.graph_theory, name='graph_theory'),
+    path('algorithms/greedy', views.greedy, name='greedy'),
+    path('algorithms/implementation', views.implementation, name='implementation'),
+    path('algorithms/np_complete', views.np_complete, name='np_complete'),
+    path('algorithms/recursion', views.recursion, name='recursion'),
+    path('algorithms/search', views.search, name='search'),
+    path('algorithms/sorting', views.sorting, name='sorting'),
+    path('algorithms/strings', views.strings, name='strings'),
+    path('algorithms/warmup', views.warmup, name='warmup'),
 
 ]
--- practice/views.py
+++ practice/views.py
@@ -8,8 +8,60 @@ def index(request):
 def algorithms(request):
     return render(request, 'practice/algorithms.html', {})
 
-def algorithms1(request):
-    return render(request, 'practice/algorithms1.html', {})
+def constructive_algorithms(request):
+    return render(request, 'practice/algorithms/constructive_algorithms.html', {
+    "subdomain": 9,
+    })
+
+def dynamic_programming(request):
+    return render(request, 'practice/algorithms/dynamic_programming.html', {
+    "subdomain": 7,
+    })
+
+def graph_theory(request):
+    return render(request, 'practice/algorithms/graph_theory.html', {
+    "subdomain": 5,
+    })
+
+def greedy(request):
+    return render(request, 'practice/algorithms/greedy.html', {
+    "subdomain": 6,
+    })
+
+def implementation(request):
+    return render(request, 'practice/algorithms/implementation.html', {
+    "subdomain": 1,
+    })
+
+def np_complete(request):
+    return render(request, 'practice/algorithms/np_complete.html', {
+    "subdomain": 10,
+    })
+
+def recursion(request):
+    return render(request, 'practice/algorithms/recursion.html', {
+    "subdomain": 8,
+    })
+
+def search(request):
+    return render(request, 'practice/algorithms/search.html', {
+    "subdomain": 4,
+    })
+
+def sorting(request):
+    return render(request, 'practice/algorithms/sorting.html', {
+    "subdomain": 3,
+    })
+
+def strings(request):
+    return render(request, 'practice/algorithms/strings.html', {
+    "subdomain": 2,
+    })
+
+def warmup(request):
+    return render(request, 'practice/algorithms/warmup.html', {
+    "subdomain": 0,
+    })
 
 def data_structures(request):
     return render(request, 'practice/data_structures.html', {})
@@ -22,13 +74,3 @@ def java(request):
 
 def c(request):
     return render(request, 'practice/c.html', {})
-
-def codemirror(request):
-    return render(request, 'practice/codemirror.html', {})
-
-def solution(request):
-    if request.method == 'POST':
-        url = request.POST.get('url')
-    return render(request, 'practice/solution.html', {
-    "url":url
-    })
