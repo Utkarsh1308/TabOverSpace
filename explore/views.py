9dc98e1bf1acb1e508c295fd01c9d8fec26cf16b
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2018-12-27 22:47:15 -0800

    Fix Explore and Practice Page
    Fixes the subdomains links and ui
    Adds Practice section in navbar
    Removes unnecessary html files in Practice app
    Adds pages for subtopics in expore section
    Fixes background images for articles in explore
    
    Closes https://github.com/Utkarsh1308/TabOverSpace/issues/35

--- explore/views.py
+++ explore/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'explore/index.html', {})
+
+def ai(request):
+    return render(request, 'explore/ai.html', {
+    "subdomain": 0,
+    })
+
+def big_data_analytics(request):
+    return render(request, 'explore/big_data_analytics.html', {
+    "subdomain": 2,
+    })
+
+def bioinformatics(request):
+    return render(request, 'explore/bioinformatics.html', {
+    "subdomain": 3,
+    })
+
+def cryptography(request):
+    return render(request, 'explore/cryptography.html', {
+    "subdomain": 5,
+    })
+
+def cyber_security(request):
+    return render(request, 'explore/cyber_security.html', {
+    "subdomain": 1,
+    })
+
+def machine_learning(request):
+    return render(request, 'explore/machine_learning.html', {
+    "subdomain": 4,
+    })
