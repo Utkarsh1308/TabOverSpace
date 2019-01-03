8ef5491e0015b1c940a22fcb7a4c558e246383c6
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2019-01-02 23:51:03 -0800

    Fixed links on hime, leaderboard and explore pages
    Fixes https://github.com/Utkarsh1308/TabOverSpace/issues/46

--- explore/urls.py
+++ explore/urls.py
@@ -5,10 +5,4 @@ app_name="explore"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ai', views.ai, name='ai'),
-    path('big_data_analytics', views.big_data_analytics, name='big_data_analytics'),
-    path('bioinformatics', views.bioinformatics, name='bioinformatics'),
-    path('cryptography', views.cryptography, name='cryptography'),
-    path('cyber_security', views.cyber_security, name='cyber_security'),
-    path('machine_learning', views.machine_learning, name='machine_learning'),
 ]
--- explore/views.py
+++ explore/views.py
@@ -5,33 +5,3 @@ def index(request):
     return render(request, 'explore/index.html', {
     "subdomain": 0,
     })
-
-def ai(request):
-    return render(request, 'explore/ai.html', {
-    "subdomain": 0,
-    })
-
-def big_data_analytics(request):
-    return render(request, 'explore/big_data_analytics.html', {
-    "subdomain": 2,
-    })
-
-def bioinformatics(request):
-    return render(request, 'explore/bioinformatics.html', {
-    "subdomain": 3,
-    })
-
-def cryptography(request):
-    return render(request, 'explore/cryptography.html', {
-    "subdomain": 5,
-    })
-
-def cyber_security(request):
-    return render(request, 'explore/cyber_security.html', {
-    "subdomain": 1,
-    })
-
-def machine_learning(request):
-    return render(request, 'explore/machine_learning.html', {
-    "subdomain": 4,
-    })
