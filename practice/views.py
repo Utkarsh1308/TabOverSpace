68c818000ca2fcf0dd0d3ee8bd6e3b6d209c9561
Utkarsh Sharma <f20160600@goa.bits-pilani.ac.in>
2019-12-23 17:13:39 +0530

    Integrate Jdoodle API (#119)

--- practice/views.py
+++ practice/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Track, Subdomain, Question
 
+import requests, json
+
 # Create your views here.
 def index(request):
     return render(request, 'practice/index.html', {})
@@ -53,4 +55,34 @@ def question(request, subdomain, question, track):
     'track': track,
     'question': question,
     }
+
+    if request.method == "POST":
+        username = request.POST['username']
+        if username is not None:
+
+            url = 'https://api.jdoodle.com/v1/execute'
+
+            # API credentials are globally visible. Needs to be changed before
+            # deploying
+            params = {
+                'script': username,
+                'language': 'python3',
+                'versionIndex': '1',
+                'clientId': '3265e3f531417e92189918053074fb26',
+                'clientSecret': '8dfde96ed1da61c9f8197b6b01933e164ba5569af54efc182dfd510b26322880'
+            }
+
+            query = jdoodle(url, params)
+
+            context['query'] = query['output']
+
+            return render(request, 'practice/question.html', context)
+
     return render(request, 'practice/question.html', context)
+
+def jdoodle(url, params):
+    response = requests.get(url, json=params)
+
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return {'output': 'Failure'}
